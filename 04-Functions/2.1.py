b37f0c8efd60cb91182e4332ce4a43967d17e6d2
mikolajpasieka <mikolajpasieka0@gmail.com>
2025-01-04 15:01:08 +0100

    all done

---
+++ 04-Functions/2.1.py
@@ -0,0 +1,19 @@
+###
+# Program for testing built-in functions
+#
+max_number = max(7,5,6,3,8,2)
+print('Max number of 7,5,6,3,8,2 is', max_number)
+
+min_number = min(4,7,2,3,9,8)
+print('Min number of 4,7,2,3,9,8 is', min_number)
+
+str_length = len("computer science")
+print('The number of characters in "computer science" is', str_length)
+
+keybord_letter = input("Enter letter")
+print("letter read from the keyboard is", keybord_letter)
+
+str_num = chr(20303)
+print(f"number representing the string '20303' is {str_num}")
+
+bin_num = bin(304)
